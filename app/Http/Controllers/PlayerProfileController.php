606df5b9997857677ff63456c0f0065beee35aa7
Julien Appelle <julien.appelle@wanadoo.fr>
2026-07-11 11:04:24 +0200

    Correction doublon PlayerProfileController

--- app/Http/Controllers/PlayerProfileController.php
+++ app/Http/Controllers/PlayerProfileController.php
@@ -2,62 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\PlayerColorPalette;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules\Password;
 
 class PlayerProfileController extends Controller
 {
     public function edit()
     {
-        return view('profile.player');
+        return view('profile.player', [
+            'playerColors' => PlayerColorPalette::colors(),
+        ]);
     }
 
     public function update(Request $request)
     {
         $user = $request->user();
 
+        $request->merge([
+            'nickname' => strtoupper((string) $request->input('nickname')),
+            'color' => strtoupper((string) $request->input('color')),
+        ]);
+
         $data = $request->validate([
             'nickname' => [
                 'required',
                 'string',
-                'regex:/^[A-Za-z]{2}[0-9]{2}$/',
+                'regex:/^[A-Z]{2}[0-9]{2}$/',
                 Rule::unique('users', 'nickname')->ignore($user->id),
             ],
-
             'email_pro' => [
                 'nullable',
                 'email',
                 'required_without:email_perso',
                 Rule::unique('users', 'email_pro')->ignore($user->id),
             ],
-
             'email_perso' => [
                 'nullable',
                 'email',
                 'required_without:email_pro',
                 Rule::unique('users', 'email_perso')->ignore($user->id),
             ],
-
             'color' => [
                 'required',
-                'regex:/^#[0-9A-Fa-f]{6}$/',
+                'string',
+                Rule::in(PlayerColorPalette::colors()),
                 Rule::unique('users', 'color')->ignore($user->id),
             ],
-
-            'current_password' => ['nullable', 'required_with:password', 'current_password'],
-            'password' => ['nullable', 'confirmed', Password::defaults()],
         ]);
-
-        if (! empty($data['password'])) {
-            $data['password'] = Hash::make($data['password']);
-        } else {
-            unset($data['password']);
-        }
-
-        unset($data['current_password']);
-        unset($data['password_confirmation']);
 
         $data['email'] = $data['email_pro'] ?? $data['email_perso'] ?? null;
 
